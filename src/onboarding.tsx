ed8065efdf958aa10386644e347ab9012929fc12
AnieX9009 <animeshmkpa@gmail.com>
2024-12-07 11:57:47 +0530

    first commit

---
+++ src/onboarding.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  Image,
+  View,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Onboarding() {
+  const navigation = useNavigation();
+
+  return (
+    <View>
+     <Text>Hiii</Text>
+    </View>
+
+
+
+  );
+};
